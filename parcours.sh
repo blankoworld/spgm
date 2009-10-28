6ff353a91e1b9fce110c023e78c8b9281e6c603b
Olivier DOSSMANN <blankoworld@wanadoo.fr>
2009-10-28 14:40:36 +0100

    Ajout des commentaires dans le script de parcours

--- parcours.sh
+++ parcours.sh
@@ -37,49 +37,82 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.If not, see <http://www.gnu.org/licenses/>.
 
+## DEBUT / BEGIN
+
+## Initialisation des variables
 REP_SOURCE=$1
+
+## Parcours recursifs dans chacun des dossiers
 find ${REP_SOURCE} -type d | while read A ; do
-	echo $A
-	echo -e "\tSuppression des fichiers indesirables"
-	# Mettre ici les tests et les suppressions des fichiers non - desires
-	# Ici ce sont des fichiers index.dat resultant de phpwebgallery
-#	if test -f ${A}/index.dat
-#	then
-#		rm -f ${A}/index.dat
-#	fi
-	echo -e "\tConversion des images"
-	for i in `ls ${A} | grep -Ei "*.jpg|*.jpeg|*.bmp|*.png|*.gif" | grep -v _thb_`
-	do 
-		if test -f ${A}/$i && ! test -a ${A}/_thb_$i
-		then
-			convert -resize 120 ${A}/$i ${A}/_thb_$i
-		fi
-	done
-	echo -e "\tCreation descriptif galerie"
-	if ! test -f ${A}/gal-desc.txt
-	then
-		touch ${A}/gal-desc.txt
-		echo "Aucune description" > ${A}/gal-desc.txt
-	fi
-	echo -e "\tCreation descriptif images"
-	if ! test -f ${A}/pic-desc.txt
-	then
-		echo -e "\t\tCreation du fichier de description"
-		touch ${A}/pic-desc.txt
-		echo -e "\t\tAjout de commentaires placebo"
-		echo -e "; Do not remove this comment (used for UTF-8 compliance)\n" > ${A}/pic-desc.txt
-		for i in `ls ${A} | grep -Ei "*.jpg|*.jpeg|*.bmp|*.png|*.gif" | grep "_thb_"`
-		do
-			echo "$i | Aucun commentaire" >> ${A}/pic-desc.txt
-		done
-	else
-		echo -e "\t\tVerification du fichier de description"
-		for i in `ls ${A} | grep -Ei "*.jpg|*.jpeg|*.bmp|*.png|*.gif" | grep "_thb_"`
-		do
-			if  [ -z "$(grep $i ${A}/pic-desc.txt)" ]
-			then
-				echo "$i | Aucun commentaire" >> ${A}/pic-desc.txt
-			fi
-		done
-	fi
+
+  # Affichage dossier dans lequel nous nous trouvons
+  echo $A
+
+##########
+# Mettre ici les tests et les suppressions des fichiers non - desires
+# Ici ce sont des fichiers index.dat resultant de phpwebgallery
+
+# Add here some test code, for an example to remove some unwanted files
+# Here I remove index.dat files inherit from phpwebgallery
+
+#  echo -e "\tSuppression des fichiers indesirables"
+#  if test -f ${A}/index.dat
+#  then
+#    rm -f ${A}/index.dat
+#  fi
+
+##########
+
+  ## Conversion des images en miniatures
+  echo -e "\tConversion des images"
+  # On se concentre sur les fichiers images dont les formats sont listes \
+  # ci-dessous mais PAS les miniatures
+  for i in `ls ${A} | grep -Ei "*.jpg|*.jpeg|*.bmp|*.png|*.gif" | grep -v _thb_`
+  do
+    # Si l'element est un fichier (et qu'il existe) ET que son miniature \
+    # n'existe pas
+    if test -f ${A}/$i && ! test -a ${A}/_thb_$i
+    then # alors conversion
+      convert -resize 120 ${A}/$i ${A}/_thb_$i
+    fi
+  done
+
+  ## Creation d'un descriptif de la galerie
+  echo -e "\tCreation descriptif galerie"
+  # Si gal-desc.txt n'existe pas
+  if ! test -a ${A}/gal-desc.txt
+  then # alors creation du fichier gal-desc.txt
+    touch ${A}/gal-desc.txt
+    echo "Aucune description" > ${A}/gal-desc.txt
+  fi
+
+  ## Creation d'un descriptif des images
+  echo -e "\tCreation descriptif images"
+  # Si pic-desc.txt n'existe pas
+  if ! test -a ${A}/pic-desc.txt
+  then # alors creation et completement du fichier pic-desc.txt
+    echo -e "\t\tCreation du fichier de description"
+    touch ${A}/pic-desc.txt
+    echo -e "\t\tAjout de commentaires placebo"
+    echo -e "; Do not remove this comment (used for UTF-8 compliance)\n" > ${A}/pic-desc.txt
+    # Parcours des fichiers images selon les formats listes ci-dessous mais \
+    # SEULEMENT les miniatures cette fois-ci
+    for i in `ls ${A} | grep -Ei "*.jpg|*.jpeg|*.bmp|*.png|*.gif" | grep "_thb_"`
+    do
+      echo "$i | Aucun commentaire" >> ${A}/pic-desc.txt
+    done
+  else # s'il existe
+    echo -e "\t\tVerification du fichier de description"
+    # Comme avant : parcours des miniatures du dossier
+    for i in `ls ${A} | grep -Ei "*.jpg|*.jpeg|*.bmp|*.png|*.gif" | grep "_thb_"`
+    do
+      # Si la valeur resultante de la recherche sur le fichier image est nulle
+      if  [ -z "$(grep $i ${A}/pic-desc.txt)" ]
+      then # alors ajout dans l'index pic-desc.txt
+        echo "$i | Aucun commentaire" >> ${A}/pic-desc.txt
+      fi
+    done
+  fi
+
+  ## FIN / END
 done
